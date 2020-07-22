1628036683c1999440f8d7c172b600b357402413
willianmendesf <robertinho1190@gmail.com>
2020-07-22 18:28:37 -0300

    RocketSeat App

---
+++ Mobile/RocketSeat/ReactNative/hunt/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Routes from './routes'
+
+const App = () => {
+	return (
+		<View>
+			<Text>
+				<Routes></Routes>
+				App
+			</Text>
+		</View>
+	)
+}
+
+export default App;
