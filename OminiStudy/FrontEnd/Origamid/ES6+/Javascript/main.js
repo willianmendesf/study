67721cb1e3951a93c30c91169c906b82033c5fcf
willianmendesf <robertinho1190@gmail.com>
2020-07-24 00:35:37 -0300

    Ended Backend First Step

---
+++ OminiStudy/FrontEnd/Origamid/ES6+/Javascript/main.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function Project () {
+	this.id = id;
+	this.title = title;
+	this.owner = owner;
+}
+
+const project = new Project ()
+project.id = 1;
+project.title = "William";
+project.owner = "casameto";
+
+
+console.log(project)
