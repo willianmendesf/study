2ce5d3416ff977f55de5367722c1fb716b9cba90
willianmendesf <robertinho1190@gmail.com>
2020-08-04 01:57:09 -0300

    ReactNative Structures

--- OminiBlade/Mobile/src/index.js
+++ OminiBlade/Mobile/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+
+// Não possuem valor semântico (significado) (São abstratos).
+// Não possuem estilização própria.
 
 // View: div, footer, header, main, aside, section.
+// Text: p, span, strong, h1, h2...
 
 export default function App () {
-	return <View/>
+	return <View/>;
 }
+
+const styles = StyleSheet.create({
+	container: {
+		properts:'values'
+	}
+})
