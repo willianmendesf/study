1102304c3bae2ee15da69a606ed5e50e9bc89cc4
willianmendesf <robertinho1190@gmail.com>
2020-12-28 18:13:15 -0300

    React: n2 componentizando o sistema;

--- OminiStudy/FrontEnd/Alura/ReactJS/ceep/src/components/ListaDeNotas.js
+++ OminiStudy/FrontEnd/Alura/ReactJS/ceep/src/components/ListaDeNotas.js
@@ -5,15 +5,20 @@ class ListaDeNotas extends Component {
 	render() {
 		return (
 			<ul>
-				<li>
-					<CardNota/>
-				</li>
-				<li>
-					<CardNota/>
-				</li>
-				<li>
-					<CardNota/>
-				</li>
+				{Array.of("Trabalho", "Trabalho", "Estudos").map(item => {
+					return (
+						<li>
+							<section>
+								<header>
+									<h3>{item}</h3>
+								</header>
+								<body>
+									<p>Escreva sua nota</p>
+								</body>
+							</section>
+						</li>
+					)
+				})}
 			</ul>
 		)
 	}
