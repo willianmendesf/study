c2e4cc3d52430318865fab96e6e36df64adc8c35
willianmendesf <robertinho1190@gmail.com>
2021-05-14 00:17:38 -0300

    backend-java: java excessões

--- OminiStudy/BackEnd/java/eclipse-workspace/java-pilha/src/Fluxo.java
+++ OminiStudy/BackEnd/java/eclipse-workspace/java-pilha/src/Fluxo.java
@@ -2,7 +2,13 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+        	metodo1();
+        } catch(ArithmeticException ex) {
+        	// String msg = ex.getMessage();
+        	// System.out.println("ArithmeticException " + msg);
+        	ex.printStackTrace();
+        }
         System.out.println("Fim do main");
     }
 
@@ -15,7 +21,10 @@ public class Fluxo {
     private static void metodo2() {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
+            System.out.println(i + "*");
+            // int a = i / 0;        
+            Conta c = null;
+            c.deposita();
         }
         System.out.println("Fim do metodo2");
     }
