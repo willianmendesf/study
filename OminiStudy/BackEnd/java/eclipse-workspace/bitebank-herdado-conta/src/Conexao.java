c96aea89955509b59150f209d60693997126aaa5
willianmendesf <robertinho1190@gmail.com>
2021-05-19 20:10:16 -0300

    backend-java: End java excessões

--- OminiStudy/BackEnd/java/eclipse-workspace/bitebank-herdado-conta/src/Conexao.java
+++ OminiStudy/BackEnd/java/eclipse-workspace/bitebank-herdado-conta/src/Conexao.java
@@ -1,4 +1,4 @@
-public class Conexao {
+public class Conexao implements AutoCloseable {
 
     public Conexao() {
         System.out.println("Abrindo conexao");
@@ -9,7 +9,8 @@ public class Conexao {
         throw new IllegalStateException();
     }
 
-    public void fecha() {
-        System.out.println("Fechando conexao");
-    }
+	@Override
+	public void close() {
+		System.out.println("Fechando conexao");
+	}
 }
--- OminiStudy/BackEnd/java/eclipse-workspace/bitebank-herdado-conta/src/TesteConexao.java
+++ OminiStudy/BackEnd/java/eclipse-workspace/bitebank-herdado-conta/src/TesteConexao.java
@@ -1,13 +1,26 @@
 public class TesteConexao {
 	public static void main(String[] args) {
-		Conexao con = null;
-	    try {
-	    	con = new Conexao();
-	        con.leDados();
-	    } catch(Exception ex) {
-	        System.out.println("Deu erro na conexao");
-	    } finally {
-	    	con.fecha();	    	
-	    }
+		
+		
+		try(Conexao conexao = new Conexao()){
+			conexao.leDados();
+		} catch (Exception ex) {
+			ex.getMessage();
+		}
+		
+		
+		
+		
+//		Conexao con = null;
+//		try {
+//	    	con = new Conexao();
+//	        con.leDados();
+//	    } catch(IllegalStateException ex) {
+//	        System.out.println("Deu erro na conexao");
+//	    } finally {
+//	    	if(con != null) {
+//	    		con.close();	    		
+//	    	}
+//	    }
 	}
 }
