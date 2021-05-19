fffa32b063298768f7b6c02e97284296b17b94b6
willianmendesf <robertinho1190@gmail.com>
2021-05-19 15:20:23 -0300

    backend-java: java excessões -old try style

---
+++ OminiStudy/BackEnd/java/eclipse-workspace/bitebank-herdado-conta/src/TesteConexao.java
@@ -0,0 +1,13 @@
+public class TesteConexao {
+	public static void main(String[] args) {
+		Conexao con = null;
+	    try {
+	    	con = new Conexao();
+	        con.leDados();
+	    } catch(Exception ex) {
+	        System.out.println("Deu erro na conexao");
+	    } finally {
+	    	con.fecha();	    	
+	    }
+	}
+}
