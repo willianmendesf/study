e8ca5832aa26dfb32e9b83bfe06a601d0d1afba1
willianmendesf <robertinho1190@gmail.com>
2021-06-21 23:00:06 -0300

    remove Javascript interface heranças project alura

--- OminiStudy/FrontEnd/Alura/JavaScript:InterfaceseHerançaemOrientaçãoaObjetos/Funcionarios/Gerente.js
+++
@@ -1,8 +0,0 @@
-import { Funcionarios } from "./Funcionarios.js";
-
-export class Gerente extends Funcionarios {
-	constructor(nome, salario, cpf) {
-		super(nome, salario, cpf);
-		this._bonificacao = 1.1
-	}
-}
