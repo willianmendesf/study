6c146e5bfccce3f95b38f47bd5c9ca3661d2393f
willianmendesf <robertinho1190@gmail.com>
2021-07-18 22:41:10 -0300

    [Spring+Angular] - Get resource in url of request

--- OminiStudy/BackEnd/java/eclipse-workspace/algamoney-api/src/main/java/com/example/algamoney/api/controller/CategoryController.java
+++ OminiStudy/BackEnd/java/eclipse-workspace/algamoney-api/src/main/java/com/example/algamoney/api/controller/CategoryController.java
@@ -2,16 +2,17 @@ package com.example.algamoney.api.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -31,10 +32,10 @@ public class CategoryController {
 	}
 	
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void create(@RequestBody Category category, HttpServletResponse response) {
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
 		// função para recuperar o novo recurso e criar uma URL onde o consumidor pode encontrar esse novo recurso.
 		Category saveCategory = categoryRepository.save(category);
+		
 		URI uri = ServletUriComponentsBuilder //helper spring
 					.fromCurrentRequestUri() //A partir da requisição atual (pegar a url do servidor em questão)
 					.path("/{id}")	// usar o id
@@ -42,6 +43,13 @@ public class CategoryController {
 					.toUri(); // usar o ID na area informada
 		
 		response.setHeader("Location", uri.toASCIIString()); // setar o localtion com a URI buscada e montada.
+		
+		return ResponseEntity.created(uri).body(saveCategory);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Category> getId(@PathVariable Long id) {
+		return categoryRepository.findById(id);
 	}
 }
 
