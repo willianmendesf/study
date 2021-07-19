e962bde8d6886ae1e3f0df8f333b0931899c5ee2
willianmendesf <robertinho1190@gmail.com>
2021-07-18 22:26:18 -0300

    [Spring+Angular] - Create and pass resource in header later post requisition

--- OminiStudy/BackEnd/java/eclipse-workspace/algamoney-api/src/main/java/com/example/algamoney/api/controller/CategoryController.java
+++ OminiStudy/BackEnd/java/eclipse-workspace/algamoney-api/src/main/java/com/example/algamoney/api/controller/CategoryController.java
@@ -1,6 +1,9 @@
 package com.example.algamoney.api.controller;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Category;
 import com.example.algamoney.api.repository.CategoryRepository;
@@ -28,7 +32,18 @@ public class CategoryController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public void create(@RequestBody Category category) {
-		categoryRepository.save(category);
+	public void create(@RequestBody Category category, HttpServletResponse response) {
+		// função para recuperar o novo recurso e criar uma URL onde o consumidor pode encontrar esse novo recurso.
+		Category saveCategory = categoryRepository.save(category);
+		URI uri = ServletUriComponentsBuilder //helper spring
+					.fromCurrentRequestUri() //A partir da requisição atual (pegar a url do servidor em questão)
+					.path("/{id}")	// usar o id
+					.buildAndExpand(saveCategory.getId()) // pegar o ID na base
+					.toUri(); // usar o ID na area informada
+		
+		response.setHeader("Location", uri.toASCIIString()); // setar o localtion com a URI buscada e montada.
 	}
 }
+
+
+
