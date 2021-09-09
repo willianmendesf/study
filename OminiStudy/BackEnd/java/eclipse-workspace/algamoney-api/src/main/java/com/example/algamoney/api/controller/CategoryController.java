40f788b735e8f393530f83371c4dff9b04c69172
willianmendesf <robertinho1190@gmail.com>
2021-09-08 22:01:25 -0300

    new project in spring

--- OminiStudy/BackEnd/java/eclipse-workspace/algamoney-api/src/main/java/com/example/algamoney/api/controller/CategoryController.java
+++ OminiStudy/BackEnd/java/eclipse-workspace/algamoney-api/src/main/java/com/example/algamoney/api/controller/CategoryController.java
@@ -1,52 +1,50 @@
 package com.example.algamoney.api.controller;
 
-import com.example.algamoney.api.model.Category;
-import com.example.algamoney.api.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algamoney.api.model.Category;
+import com.example.algamoney.api.repository.CategoryRepository;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
 
 	@Autowired
-	private CategoryRepository categoryRepository;
+	private CategoryRepository repository;
 
 	@GetMapping
 	public List<Category> listAll() {
-		return categoryRepository.findAll();
+		return repository.findAll();
 	}
 
 	@PostMapping
 	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
-		// função para recuperar o novo recurso e criar uma URL onde o consumidor pode encontrar esse novo recurso.
-		Category saveCategory = categoryRepository.save(category);
+		Category saveCategory = repository.save(category);
 
-		URI uri = ServletUriComponentsBuilder //helper spring
-					.fromCurrentRequestUri() //A partir da requisição atual (pegar a url do servidor em questão)
-					.path("/{id}")	// usar o id
-					.buildAndExpand(saveCategory.getId()) // pegar o ID na base
-					.toUri(); // usar o ID na area informada
+		URI uri = ServletUriComponentsBuilder
+			.fromCurrentRequestUri()
+				.path("/{id}")
+					.buildAndExpand(saveCategory.getId())
+						.toUri();
 
-		response.setHeader("Location", uri.toASCIIString()); // setar o localtion com a URI buscada e montada.
+		response.setHeader("Location", uri.toASCIIString());
 
 		return ResponseEntity.created(uri).body(saveCategory);
 	}
-
-	@GetMapping("/{id}")
-	public Object getById(@PathVariable Long id) {
-		if (id == null) {
-			return ResponseEntity.notFound().build();
-		} else {
-			return categoryRepository.findById(id);
-		}
-	}
+	
+	
 }
 
 
--- OminiStudy/BackEnd/java/eclipse-workspace/algamoney-api/src/main/java/com/example/algamoney/api/repository/CategoryRepository.java
+++ OminiStudy/BackEnd/java/eclipse-workspace/algamoney-api/src/main/java/com/example/algamoney/api/repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.example.algamoney.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Category;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-	
+
 }
